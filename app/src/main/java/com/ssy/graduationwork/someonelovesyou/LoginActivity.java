39f4d3af3c4304e92ae7ab0d80a2febf65109d2c
jmandy <jmandy@naver.com>
2018-07-17 19:41:21 +0900

    login,signup
    activity

---
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.ssy.graduationwork.someonelovesyou;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button button_signup, button_login;
+    EditText editText_phone, getEditText_phone_pw;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button_signup = findViewById(R.id.button_signup);
+        button_login = findViewById(R.id.button_login);
+        editText_phone = findViewById(R.id.editText_phone);
+        getEditText_phone_pw = findViewById(R.id.editText_pw);
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input_phone = editText_phone.getText().toString();
+                String input_pw = getEditText_phone_pw.getText().toString();
+                if (input_phone.equalsIgnoreCase("")) {
+                    // Toast.makeText(this,"d",Toast.LENGTH_SHORT).show();
+                    //  Toast.makeText(this, "폰번호 입력하세요", Toast.LENGTH_SHORT).show();
+                } else if (input_pw.equalsIgnoreCase("")) {
+                    //    Toast.makeText(this, "비밀번호 입력하세요", Toast.LENGTH_SHORT).show();
+                }
+
+                Intent MainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(MainIntent);
+            }
+        });
+        // button_signup.setOnClickListener(this);
+        //  button_login.setOnClickListener(this);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/SignupActivity.java
@@ -0,0 +1,26 @@
+package com.ssy.graduationwork.someonelovesyou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        final EditText edit_name=findViewById(R.id.editText_name);
+        final EditText edit_phone=findViewById(R.id.editText_newPhone);
+        final EditText edit_email=findViewById(R.id.editText_email);
+        final EditText edit_pw=findViewById(R.id.editText_newPW);
+        final EditText edit_pwCheck=findViewById(R.id.editText_checkPW);
+
+        Button registerButton=findViewById(R.id.button_createAccount);
+
+
+
+    }
+}
