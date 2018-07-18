8d0e2a9aaeac9b8164edf0a281e1f6b9b591a029
YJH <altos0120@hanmail.net>
2018-07-18 21:31:02 +0900

    List View for 4 part(Friend, Heart, Who, History

--- app/src/main/java/com/ssy/graduationwork/someonelovesyou/PlantHistory.java
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/PlantHistory.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlantHistory extends Fragment {
+
+    ListView listView;
+    ListViewAdapterForHistory adapter;
+
+    ArrayList<ListViewItemForHistory> itemList;
 
 
     public PlantHistory() {
@@ -23,7 +35,47 @@ public class PlantHistory extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_plant_history, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_plant_history, null);
+
+        // 어레이리스트 새로 생성.
+        itemList = new ArrayList<ListViewItemForHistory>();
+
+        //Adapter 생성
+        adapter = new ListViewAdapterForHistory(itemList);
+
+        // 리스트뷰 참조 및 adpater 달기
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        try {
+            InputStream is  = getResources().openRawResource(R.raw.history);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String[] temp = new String[4];
+            // temp 0: 순서, 1:꽃종류, 2:시작날짜 3:끝날짜
+            while ((br.readLine()) != null) { // 책 사이의 개행(빈 줄 하나) 읽기
+
+                for(int i = 0; i < 4; i++) {
+                    temp[i] = br.readLine();
+                }
+
+                String order = temp[0];
+                String flowerImgName = temp[1];
+
+                int flowerImgResId = getResources().getIdentifier(flowerImgName, "drawable", "com.ssy.graduationwork.someonelovesyou");
+
+                String startDate = temp[2];
+                String endDate = temp[3];
+
+                adapter.addItem(flowerImgResId, startDate, endDate);
+
+            }
+
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
 
         return rootView;
     }
--- app/src/main/java/com/ssy/graduationwork/someonelovesyou/PlantWho.java
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/PlantWho.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlantWho extends Fragment {
+
+    ListView listView;
+    ListViewAdapterForHeart adapter;
+
+    ArrayList<ListViewItemForHeart> itemList;
 
 
     public PlantWho() {
@@ -23,7 +35,52 @@ public class PlantWho extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_plant_who, container, false);
+        //View rootView =  inflater.inflate(R.layout.fragment_plant_who, container, false);
+
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_plant_who, null);
+
+        // 어레이리스트 새로 생성.
+        itemList = new ArrayList<ListViewItemForHeart>();
+
+        //Adapter 생성
+        adapter = new ListViewAdapterForHeart(itemList);
+
+        // 리스트뷰 참조 및 adpater 달기
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        try {
+            InputStream is  = getResources().openRawResource(R.raw.who);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String[] temp = new String[5];
+            // temp 0: 폰번호(이미지이름), 1: 이름, 2: 날짜, 3: 오전/후, 4: 시간
+            while ((br.readLine()) != null) { // 책 사이의 개행(빈 줄 하나) 읽기
+
+                for(int i = 0; i < 5; i++) {
+                    temp[i] = br.readLine();
+                }
+
+                //이미지이름==폰번호
+                String personImgName = "ph" + temp[0];
+
+                int personImgResId = getResources().getIdentifier(personImgName, "drawable", "com.ssy.graduationwork.someonelovesyou");
+
+                String name = temp[1] + "님이 당신을 생각하고 있어요";
+                String date = temp[2];
+                String ampm = temp[3];
+                String time = temp[4];
+
+                adapter.addItem(personImgResId, name, date, ampm, time);
+
+            }
+
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
 
         return rootView;
     }
