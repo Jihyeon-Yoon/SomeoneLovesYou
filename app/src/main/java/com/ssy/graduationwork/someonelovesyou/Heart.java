a76549166fe07e7b6161916bdf55621561624de3
YJH <altos0120@hanmail.net>
2018-07-20 14:02:58 +0900

    Add Title & Sorting in Heart Part

--- app/src/main/java/com/ssy/graduationwork/someonelovesyou/Heart.java
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/Heart.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
@@ -12,6 +13,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by YJH on 2018-07-17.
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 
 public class Heart extends Fragment {
 
+    Button timeSortBtn, nameSortBtn;
     ListView listView;
     ListViewAdapterForHeart adapter;
 
@@ -72,6 +76,86 @@ public class Heart extends Fragment {
         } catch(IOException e) {
             e.printStackTrace();
         }
+
+
+        // 시간순으로 정렬하는 버튼, 버튼을 누르면 시간순으로 정렬한다.
+        timeSortBtn = rootView.findViewById(R.id.btn_sort_time);
+        timeSortBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Comparator<ListViewItemForHeart> timeAsc = new Comparator<ListViewItemForHeart>() {
+                    @Override
+                    public int compare(ListViewItemForHeart item1, ListViewItemForHeart item2) {
+                        String time1 = "";
+                        String time2 = "";
+                        String temp = "";
+
+                        //time1 얻기
+                        temp = item1.getDate();
+                        time1 = temp.substring(0, 4) + temp.substring(5, 7) + temp.substring(8);
+
+                        if(item1.getAMPM().equals("오후")) {
+                            String strBuffer="";
+                            int intBuffer=0;
+
+                            temp = item1.getTime();
+                            strBuffer = temp.substring(0, 2) + temp.substring(3);
+                            intBuffer = Integer.parseInt(strBuffer);
+                            intBuffer += 1200;
+                            strBuffer = String.valueOf(intBuffer);
+                            time1 += strBuffer;
+
+                        } else {
+                            temp = item1.getTime();
+                            time1 += temp.substring(0, 2) + temp.substring(3);
+                        }
+
+                        temp = "";
+
+                        //time2 얻기
+                        temp = item2.getDate();
+                        time2 = temp.substring(0, 4) + temp.substring(5, 7) + temp.substring(8);
+
+                        if(item2.getAMPM().equals("오후")) {
+                            String strBuffer="";
+                            int intBuffer=0;
+
+                            temp = item2.getTime();
+                            strBuffer = temp.substring(0, 2) + temp.substring(3);
+                            intBuffer = Integer.parseInt(strBuffer);
+                            intBuffer += 1200;
+                            strBuffer = String.valueOf(intBuffer);
+                            time2 += strBuffer;
+
+                        } else {
+                            temp = item2.getTime();
+                            time2 += temp.substring(0, 2) + temp.substring(3);
+                        }
+
+                        return time1.compareTo(time2);
+                    }
+                };
+                Collections.sort(itemList, timeAsc);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // 이름순으로 정렬하는 버튼, 버튼을 누르면 이름순(가나다순))으로 정렬한다.
+        nameSortBtn = rootView.findViewById(R.id.btn_sort_name);
+        nameSortBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Comparator<ListViewItemForHeart> nameAsc = new Comparator<ListViewItemForHeart>() {
+                    @Override
+                    public int compare(ListViewItemForHeart item1, ListViewItemForHeart item2) {
+                        return item1.getName().compareTo(item2.getName());
+                    }
+                };
+
+                Collections.sort(itemList, nameAsc);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
 
         return rootView;
