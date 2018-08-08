b40dc49250fa7b563cf23d4e0d6145b7f2bd5e3b
jmandy <jmandy@naver.com>
2018-08-08 15:01:35 +0900

    MusicEmoticon searchFunction2~4
    MusicEmoticon searchFunction2~4

--- app/src/main/java/com/ssy/graduationwork/someonelovesyou/MusicEmotion3.java
+++ app/src/main/java/com/ssy/graduationwork/someonelovesyou/MusicEmotion3.java
@@ -3,10 +3,13 @@ package com.ssy.graduationwork.someonelovesyou;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
@@ -28,6 +31,9 @@ public class MusicEmotion3 extends Fragment {
     ListViewAdapterForMusic adapter;
 
     ArrayList<ListViewItemForMusic> itemList;
+    ArrayList<ListViewItemForMusic>  arraylist;
+    //검색창
+    EditText search;
 
 
     public MusicEmotion3() {
@@ -46,6 +52,7 @@ public class MusicEmotion3 extends Fragment {
 
         //Adapter 생성
         adapter = new ListViewAdapterForMusic(itemList);
+        search=rootView.findViewById(R.id.editSearch);
 
         // 리스트뷰 참조 및 adpater 달기
         listView = (ListView) rootView.findViewById(R.id.listView);
@@ -109,10 +116,64 @@ public class MusicEmotion3 extends Fragment {
             }
         });
 
+        arraylist = new  ArrayList<ListViewItemForMusic>();
+        arraylist.addAll(itemList);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+                String searchText=search.getText().toString();
+                search(searchText);
+
+
+            }
+        });
+
 
 
         return rootView;
 
+    }
+
+    public void search(String charText) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        itemList.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+            itemList.addAll(arraylist);
+        }
+        // 문자 입력을 할때..
+        else
+        {
+            // 리스트의 모든 데이터를 검색한다.
+            for(int i = 0;i < arraylist.size(); i++)
+            {
+
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist.get(i).getTitle().contains(charText))
+                {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    itemList.add(arraylist.get(i));
+
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        adapter.notifyDataSetChanged();
     }
 
 }
